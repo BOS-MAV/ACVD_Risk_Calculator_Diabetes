958fee529383e61b45715683c3258c5d77ecabb6
BOS-MAV <52087638+BOS-MAV@users.noreply.github.com>
2020-11-25 00:44:41 -0500

    Update riskCalc.js

--- riskCalc.js
+++ riskCalc.js
@@ -36,7 +36,6 @@ function calc_risk() {
                     sex = 1;
                 sexWeight = sex * -0.44487;
                 race_t = $("input[name = 'Race']:checked").val();
-                alert(race_t);
                 if (race_t === 'White')
                     race = 0;
                 else if (race_t === 'African American')
@@ -113,6 +112,7 @@ function calc_ASCVD()
                     sex = 1;
                 sexWeight = sex * -0.21382
                 race_t = $("input[name = 'Race']:checked").val();
+                    alert(race_t);
                 if (race_t === 'White')
                     race = 0;
                 else if (race_t === 'African American')
